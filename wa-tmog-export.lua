36fffb1dc2415a80bf2fdcad95f9093821a47e39
qreedence <147236560+qreedence@users.noreply.github.com>
2024-11-24 05:25:58 +0100

    Handle empty slots

--- wa-tmog-export.lua
+++ wa-tmog-export.lua
@@ -1,69 +1,62 @@
 function(event, ...)
     if event == "EXPORT_TMOG" then
         local transmogList = {}
+        local equippedItems = {}
+        local excludedSlots = {
+            [2] = true, [11] = true, [12] = true, [13] = true, [14] = true, [18] = true
+        }
         for slotID = 1, 19 do
-            if slotID == 2 or slotID == 11 or slotID == 12 or slotID == 13 or slotID == 14 or slotID == 18 then
-                print("Skipping slotID: " .. slotID)
-            else
-                local transmogSlot = {}
-                local slotName = TransmogUtil.GetSlotName(slotID)
-                local itemLoc = ItemLocation:CreateFromEquipmentSlot(slotID)
-                
-                transmogSlot.Slot = slotName
-                
-                local itemId = C_Item.GetItemID(itemLoc)       
-                local transmogLoc = TransmogUtil.CreateTransmogLocation(slotName, Enum.TransmogType.Appearance, Enum.TransmogModification.Main)
-                
-                if transmogLoc then
-                    local isTransmogrified, hasPending, isPendingCollected, canTransmogrify, cannotTransmogrifyReason, hasUndo, isHideVisual, texture = C_Transmog.GetSlotInfo(transmogLoc) 
-                    local slotinfo = {
-                        isTransmogrified = isTransmogrified,
-                        hasPending = hasPending,
-                        isPendingCollected = isPendingCollected,
-                        canTransmogrify = canTransmogrify,
-                        cannotTransmogrifyReason = cannotTransmogrifyReason,
-                        hasUndo = hasUndo,
-                        isHideVisual = isHideVisual,
-                        texture = texture
-                    }
-                    
-                    if not slotinfo["isTransmogrified"] then
-                        transmogSlot.ItemID = itemId
-                        transmogSlot.ItemName = "some shit"
-                    else
-                        local baseSourceID, baseVisualID, appliedSourceID, appliedVisualID, pendingSourceID, pendingVisualID, hasUndo, isHideVisual, itemSubclass = C_Transmog.GetSlotVisualInfo(transmogLoc) 
-                        local visualSlotInfo = {
-                            baseSourceID = baseSourceID,
-                            baseVisualID = baseVisualID,
-                            appliedSourceID = appliedSourceID,
-                            appliedVisualID = appliedVisualID,
-                            pendingSourceID = pendingSourceID,
-                            pendingVisualID = pendingVisualID,
-                            hasUndo = hasUndo,
-                            isHideVisual = isHideVisual,
-                            itemSubclass = itemSubclass,
-                        }
-                        
-                        local sourceInfo = C_TransmogCollection.GetSourceInfo(visualSlotInfo["appliedSourceID"])    
-                        local appliedItemTransmogInfo = C_Item.GetAppliedItemTransmogInfo(itemLoc)            
-                        local baseItemTransmogInfo = C_Item.GetBaseItemTransmogInfo(itemLoc)
-                        
-                        appearanceID = appliedItemTransmogInfo.appearanceID              
-                        transmogSlot.ItemID = sourceInfo.itemID
-                        transmogSlot.ItemName = sourceInfo.name
-                    end
-                else
-                    print("Invalid Transmog Location for slotID " .. slotID)
-                end
-                transmogList[slotID] = transmogSlot.ItemID                     
+            if GetInventoryItemID("player", slotID) and not excludedSlots[slotID] then
+                table.insert(equippedItems, slotID)
             end
         end
-        
+        for __, slotID in pairs(equippedItems)do
+            local transmogSlot = {}
+            local slotName = TransmogUtil.GetSlotName(slotID)
+            local itemLoc = ItemLocation:CreateFromEquipmentSlot(slotID)
+            transmogSlot.Slot = slotName
+            local transmogLoc = TransmogUtil.CreateTransmogLocation(slotName, Enum.TransmogType.Appearance, Enum.TransmogModification.Main)
+            if transmogLoc then
+                local isTransmogrified, hasPending, isPendingCollected, canTransmogrify, cannotTransmogrifyReason, hasUndo, isHideVisual, texture = C_Transmog.GetSlotInfo(transmogLoc) 
+                local slotinfo = {
+                    isTransmogrified = isTransmogrified,
+                    hasPending = hasPending,
+                    isPendingCollected = isPendingCollected,
+                    canTransmogrify = canTransmogrify,
+                    cannotTransmogrifyReason = cannotTransmogrifyReason,
+                    hasUndo = hasUndo,
+                    isHideVisual = isHideVisual,
+                    texture = texture
+                }
+                if not slotinfo["isTransmogrified"] then
+                    local itemId = C_Item.GetItemID(itemLoc)
+                    transmogSlot.ItemID = itemId
+                    transmogSlot.ItemName = "some shit"
+                else
+                    local baseSourceID, baseVisualID, appliedSourceID, appliedVisualID, pendingSourceID, pendingVisualID, hasUndo, isHideVisual, itemSubclass = C_Transmog.GetSlotVisualInfo(transmogLoc) 
+                    local visualSlotInfo = {
+                        baseSourceID = baseSourceID,
+                        baseVisualID = baseVisualID,
+                        appliedSourceID = appliedSourceID,
+                        appliedVisualID = appliedVisualID,
+                        pendingSourceID = pendingSourceID,
+                        pendingVisualID = pendingVisualID,
+                        hasUndo = hasUndo,
+                        isHideVisual = isHideVisual,
+                        itemSubclass = itemSubclass,
+                    }
+                    local sourceInfo = C_TransmogCollection.GetSourceInfo(visualSlotInfo["appliedSourceID"])
+                    local appliedItemTransmogInfo = C_Item.GetAppliedItemTransmogInfo(itemLoc)
+                    appearanceID = appliedItemTransmogInfo.appearanceID
+                    transmogSlot.ItemID = sourceInfo.itemID
+                    transmogSlot.ItemName = sourceInfo.name
+                end
+                transmogList[slotID] = transmogSlot.ItemID  
+            else
+                print("Invalid Transmog Location for slotID " .. slotID)
+            end 
+        end
         DevTool:AddData(transmogList, "Full Transmog List")
-        
-        local transmogStr = ""
-        local count = 0
-        local total = #transmogList   
         local transmogStr = '{"transmog":{'
         for key, value in pairs(transmogList) do
             if key and value then
